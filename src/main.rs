5002d83af7da4a042575a089f3df3f2a51bd3d90
Kevin <kvkenyon@gmail.com>
2025-08-23 15:32:22 -0500

    Update tests to use psql

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 //! main.rs
 
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000.");
+    let configuration = get_configuration().expect("Failed to read configuration file.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind port 8000.");
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,8 @@
 //! tests/health_check.rs
 
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-use zero2prod::startup::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
@@ -31,6 +32,13 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let address = spawn_app();
     let client = reqwest::Client::new();
+    let configuration = get_configuration().expect("Failed to load configuration.");
+
+    let connection_string = configuration.database.connection_string();
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
 
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -44,6 +52,13 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
