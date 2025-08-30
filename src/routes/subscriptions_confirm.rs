0a10a8f7b2674c989c91d766d46667e6a8e41224
Kevin <kvkenyon@gmail.com>
2025-08-29 23:50:57 -0500

    Refactor confirmation error flow

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,3 +1,4 @@
+use crate::routes::error_chain_fmt;
 use actix_web::{HttpResponse, http::StatusCode, web};
 use anyhow::Context;
 use sqlx::PgPool;
@@ -12,54 +13,35 @@ pub struct Parameters {
 pub async fn confirm(
     pool: web::Data<PgPool>,
     parameters: web::Query<Parameters>,
-) -> Result<HttpResponse, ConfirmError> {
+) -> Result<HttpResponse, ConfirmationError> {
     let subscriber_id = get_subscriber_id_from_token(&pool, &parameters.subscription_token)
         .await
-        .context("Failed to get subscriber id from token.")?;
-    match subscriber_id {
-        None => Err(ConfirmError::UnauthorizedError(
-            "Invalid confirmation token".into(),
-        )),
-        Some(subscriber_id) => {
-            confirm_subscriber(&pool, subscriber_id)
-                .await
-                .context("Failed to match confirm subscriber.")?;
-            Ok(HttpResponse::Ok().finish())
-        }
-    }
-}
-
-pub fn error_chain_fmt(
-    f: &mut std::fmt::Formatter<'_>,
-    e: &impl std::error::Error,
-) -> std::fmt::Result {
-    writeln!(f, "{}\n", e)?;
-    let mut current = e.source();
-    while let Some(cause) = current {
-        writeln!(f, "Caused by:\n\t{}", cause)?;
-        current = cause.source();
-    }
-    Ok(())
+        .context("Failed to get subscriber associated with the provided token.")?
+        .ok_or(ConfirmationError::UnknownToken)?;
+    confirm_subscriber(&pool, subscriber_id)
+        .await
+        .context("Failed to update subscriber status to confirmed.")?;
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[derive(thiserror::Error)]
-pub enum ConfirmError {
-    #[error("{0}")]
-    UnauthorizedError(String),
+pub enum ConfirmationError {
+    #[error("There is no subscriber associated with the provided token.")]
+    UnknownToken,
     #[error(transparent)]
     UnexpectedError(#[from] anyhow::Error),
 }
 
-impl actix_web::ResponseError for ConfirmError {
+impl actix_web::ResponseError for ConfirmationError {
     fn status_code(&self) -> actix_web::http::StatusCode {
         match self {
-            ConfirmError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            ConfirmError::UnauthorizedError(_) => StatusCode::UNAUTHORIZED,
+            ConfirmationError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ConfirmationError::UnknownToken => StatusCode::UNAUTHORIZED,
         }
     }
 }
 
-impl std::fmt::Debug for ConfirmError {
+impl std::fmt::Debug for ConfirmationError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         error_chain_fmt(f, self)
     }
@@ -69,7 +51,7 @@ impl std::fmt::Debug for ConfirmError {
 async fn get_subscriber_id_from_token(
     pool: &PgPool,
     token: &str,
-) -> Result<Option<Uuid>, GetSubscriberIdError> {
+) -> Result<Option<Uuid>, sqlx::Error> {
     let result = sqlx::query!(
         r#"
     SELECT subscriber_id FROM subscription_tokens WHERE subscription_token = $1
@@ -77,42 +59,17 @@ async fn get_subscriber_id_from_token(
         token
     )
     .fetch_optional(pool)
-    .await
-    .map_err(GetSubscriberIdError)?;
+    .await?;
     Ok(result.map(|r| r.subscriber_id))
 }
 
-#[derive(thiserror::Error)]
-#[error("A database error occurred while fetching the subscriber from the token.")]
-pub struct GetSubscriberIdError(#[source] sqlx::Error);
-
-impl std::fmt::Debug for GetSubscriberIdError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(f, self)
-    }
-}
-
 #[tracing::instrument(name = "Mark subscriber as confirmed", skip(subscriber_id, pool))]
-async fn confirm_subscriber(
-    pool: &PgPool,
-    subscriber_id: Uuid,
-) -> Result<(), ConfirmSubscriberError> {
+async fn confirm_subscriber(pool: &PgPool, subscriber_id: Uuid) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"UPDATE subscriptions SET status = 'confirmed' WHERE id = $1"#,
         subscriber_id
     )
     .execute(pool)
-    .await
-    .map_err(ConfirmSubscriberError)?;
+    .await?;
     Ok(())
-}
-
-#[derive(thiserror::Error)]
-#[error("A database error occurred while marking subscriber as confirmed.")]
-pub struct ConfirmSubscriberError(#[source] sqlx::Error);
-
-impl std::fmt::Debug for ConfirmSubscriberError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(f, self)
-    }
 }
